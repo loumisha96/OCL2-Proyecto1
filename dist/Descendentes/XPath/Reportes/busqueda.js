c68a3eca3986c131799d305a813d98989179c9cb
carlosngv <carlosngv99@hotmail.com>
2021-06-11 17:08:59 -0600

    Prueba

---
+++ dist/Descendentes/XPath/Reportes/busqueda.js
@@ -0,0 +1,14 @@
+class busqueda {
+    constructor() {
+        this.list_nodos = new Array();
+        this.lista_query = new Array();
+    }
+    getNodos(name_nodo) {
+        return this.list_nodos;
+    }
+    restrictNodos() {
+        return this.list_nodos;
+    }
+    getSelection() {
+    }
+}
