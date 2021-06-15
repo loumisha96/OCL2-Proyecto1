213af83eba7488c5b60d803a1da3d740ce1f5adf
carlosngv <carlosngv99@hotmail.com>
2021-06-15 14:12:54 -0600

    Prueb

--- dist/indexXPath.js
+++ dist/indexXPath.js
@@ -1,11 +1,6 @@
 let ast;
-<<<<<<< HEAD
 i = 0;
 
-=======
-i=0;
-let  search;
->>>>>>> 375c83ae8cf4226e0c46f2e55f6f6daeeb1eac84
 function AnalizarXpath() {
     var ta = document.getElementById(get_vent());
     var contenido = ta.value;
@@ -19,14 +14,9 @@ function AnalizarXpath() {
     //console.log(ast.tree.children[0]);
     // i=0;
 }
-<<<<<<< HEAD
 
 function query() {
     busqueda.recorrerAst(ast.tree);
-=======
-function query(){
-    search.RecorrerAst(ast.tree);
->>>>>>> 375c83ae8cf4226e0c46f2e55f6f6daeeb1eac84
 }
 
 function ASTXPATH() {
